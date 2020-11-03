9ba8bbc048f8b75886151d9ae5f312526272d695
David Wallace <dwallacemarketing@gmail.com>
2020-11-02 22:38:49 -0500

    get zomato display

--- src/App.js
+++ src/App.js
@@ -1,54 +1,56 @@
 import './App.css';
 import React, { useState, useRef, useEffect } from 'react';
+import { render } from "react-dom";
 import Zomato from './components/Zomato/Zomato';
 
 const App = () => {
+  console.log('render')
 
-  const [locationReq, setLocationReq] = useState(false);
-  const [location, setLocation] = useState();
+  const [location, setLocation] = useState('');
+  const [dataIsReturned , setdataIsReturned ] = useState(false);
 
-  const geolocate = () => {
-    if (window.navigator && window.navigator.geolocation) {
-      console.log('geolocate ran!')
-      navigator.geolocation.getCurrentPosition(onGeolocateSuccess, onGeolocateError);
+  useEffect(() => {
+    async function geolocate() {
+      if (window.navigator && window.navigator.geolocation) {
+        console.log('geolocate ran!')
+        navigator.geolocation.getCurrentPosition(onGeolocateSuccess, onGeolocateError);
+      }
     }
-  }
 
-  const onGeolocateSuccess = (position) => {
-    let { latitude, longitude } = position.coords;
-    setLocation(
-      { latitude, longitude }
-    )
-  }
-
-  const onGeolocateError = (error) => {
-    console.warn(error.code, error.message);
-    if (error.code === 1) {
-      console.log("user said no!")
-    } else if (error.code === 2) {
-      console.log("no data available")
-    } else if (error.code === 3) {
-      console.log("timeout errror")
+    async function onGeolocateSuccess (position) {
+      let { latitude, longitude } = position.coords;
+      setLocation({ latitude, longitude })
+      console.log('onGeolocateSuccess', latitude, longitude)
+      setdataIsReturned  (true)
     }
-  }
 
-  if (locationReq != true) {
-    console.log("location 1", locationReq);
-    setLocationReq(true);
-    console.log("location 2", locationReq);
+    const onGeolocateError = (error) => {
+      console.warn(error.code, error.message);
+      if (error.code === 1) {
+        console.log("user said no!")
+      } else if (error.code === 2) {
+        console.log("no data available")
+      } else if (error.code === 3) {
+        console.log("timeout errror")
+      }
+    }
     geolocate();
-  }
+  }, [])
+
+
+
 
   return (
     <div className="App">
       <div className="Container">
-        <h1>Weather API Goes here:</h1>
+        {/* <h1>Weather API Goes here:</h1> */}
         {/* Add Weather API */}
 
         {/* Add Nasa API */}
 
         <h1> Zomato Data </h1>
-        <Zomato location={location} />
+        { dataIsReturned ? <Zomato location={location} /> : <h1>Slow Down</h1>  }
+
       </div>
 
     </div>
--- src/components/Zomato/ZomatoCard.js
+++ src/components/Zomato/ZomatoCard.js
@@ -1,14 +1,36 @@
 import React from 'react';
-import {Card } from 'reactstrap';
-
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button, Row, Col
+} from 'reactstrap';
 
 const ZomatoCard = (props) => {
-
-
+    console.log(props.searchResults);
     return (
-        <div>
-            React Cards
-        </div>
+        <Row>
+            {
+                props.searchResults
+                    .map(
+                        result => {
+                            return (
+                                <Card body outline color="secondary">
+
+                                    <CardTitle>{result.restaurant.name}</CardTitle>
+                                    <CardText>
+                                        <br />
+                        Rating: {result.restaurant.user_rating.aggregate_rating} from {result.restaurant.user_rating.votes} votes.
+                                        <br />
+                        People believe this restaurant is: {result.restaurant.user_rating.rating_text}.
+                        <br />
+                        Phone Number: {result.restaurant.phone_numbers}
+                                    </CardText>
+                                    <a href={result.restaurant.menu_url} target="_blank"><Button color="success">View Menu!</Button></a>
+                                </Card>
+                            )
+                        }
+                    )
+            }
+        </Row >
     );
 };
 
